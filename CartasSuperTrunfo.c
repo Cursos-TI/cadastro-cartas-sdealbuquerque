1af235123e48cb6ec71b53f8f196166c72669b07
sdealbuquerque <salbuquerque@uri.com.br>
2025-12-18 11:37:21 +0000

    exclusao linha

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
+//cadastro-cartas-sdealbuquerque() {
+// Programa de cadastro de cartas do super trunfo (estados brasileiros)}
 
 int main() {
 
   char estado[20] = "Alagoas";
-  char carta[20] = 1;
-  char cidade[20] = "Maceio";
+  char carta = 1;
+  char cidade[25] = "Maceio";
   int populacao = 243.000; 
-  int código = 'A1'; 
+  char código[5] = "A01"; 
   int pontoturistico = 4;
   float pib = 26.000;
-  double área = 509.320;
+  float área = 509.000;
       
     printf("Digite o Estado: \n");
     scanf("%s", &estado); 
+    printf("Digite a carta: \n");
+    scanf("%d", &carta);
     printf("Digite o código: \n");
-    scanf("%d", &código);     
-    printf("Digite a Inicial: \n");
-    scanf("%s", &carta);
+    scanf("%s", &código);     
     printf("Digite a Cidade: \n");
-    scanf("%s", &cidade);     
+    scanf ("%s", &cidade);     
     printf("Digite a populacao: \n");
     scanf("%d", &populacao);   
     printf("Digite a área: \n");
-    scanf("%d", &área);
-    print("Digite o pib: \n");
+    scanf("%f", &área);
+    printf("Digite o pib: \n");
     scanf("%f", &pib);
     printf("Digite o pontoturistico: \n");
     scanf("%d", &pontoturistico);
 
+    printf("A carta: %d \n", carta);
     printf("O estado: %s\n", estado);
     printf("O Código: %s \n", código);
-    printf("A carta: %s \n", carta);
     printf("A cidade é: %s \n", cidade);
     printf("A populaçao é: %d \n", populacao);
-    printf("A área é: %d \n", área);
+    printf("A área é: %f \n", área);
     printf("O pib é: %f \n", pib);
     printf("O pontoturistico é: %d \n", pontoturistico);  
         
@@ -47,4 +45,43 @@ return 0;
 
    } 
 
+int main() {
 
+ char estado[20] = "Goiás";
+  char carta = 2;
+  char cidade[25] = "Goiânia";
+  float populacao = 1.500000; 
+  char código[5] = "G02"; 
+  int pontoturistico = 5;
+  float pib = 38.000;
+  float área = 729.000;
+
+  printf("Digite o Estado: \n");
+    scanf("%s", &estado); 
+    printf("Digite a carta: \n");
+    scanf("%d", &carta);
+    printf("Digite o código: \n");
+    scanf("%s", &código);     
+    printf("Digite a Cidade: \n");
+    scanf ("%s", &cidade);     
+    printf("Digite a populacao: \n");
+    scanf("%f", &populacao);   
+    printf("Digite a área: \n");
+    scanf("%f", &área);
+    printf("Digite o pib: \n");
+    scanf("%f", &pib);
+    printf("Digite o pontoturistico: \n");
+    scanf("%d", &pontoturistico);
+
+    printf("A carta: %d \n", carta);
+    printf("O estado: %s\n", estado);
+    printf("O Código: %s \n", código);
+    printf("A cidade é: %s \n", cidade);
+    printf("A populaçao é: %f \n", populacao);
+    printf("A área é: %f \n", área);
+    printf("O pib é: %f \n", pib);
+    printf("O pontoturistico é: %d \n", pontoturistico);
+
+
+return 0;
+}
